55b6bdb457ed6b5ce6c80be9f39a686f602fb105
Serdar Ashirov <webapis@users.noreply.github.com>
2024-07-11 17:44:26 +0500

    out

--- src/app/media/[tur]/turk-dizileri/[...slug]/page.js
+++ src/app/media/[tur]/turk-dizileri/[...slug]/page.js
@@ -1,7 +1,8 @@
 
 import SeriesList from '../../../../components/SeriesList'
-
-
+import data from '../../../../../../data/aggregated-data/tvseries.json'
+import { genreNavs } from '../../../../components/TopNavigation'
+import deaccent from '@/app/utils/deaccent.js'
 export async function generateMetadata({ params }) {
     const { tur } = params
     return {
@@ -16,4 +17,55 @@ export default function MediaPage(props) {
     const page = parseInt(slug[slug.length - 1])
 
     return <SeriesList page={page} tur={tur} />
+}
+
+
+
+
+
+export async function generateStaticParams() {
+    const cansitatePages = []
+    debugger
+    const validData = data.filter(f => f.WATCH_LINK.length > 0)
+
+
+    for (let current of genreNavs) {
+     const {slug}=current
+
+        const dataTur = slug === 'tum' ? validData : validData.filter(f => {
+
+            const exists = f.GENRES.flat(1).map(m => deaccent(m).trim().toLowerCase()).includes(slug)
+
+            return exists
+        })
+
+        //const paginatedData = paginate(dataTur, page, 50)
+        const pageCount = Math.ceil(dataTur.length / 50)
+    
+        const pages = Array.from({ length: pageCount }, (_, i) => i + 1);
+   
+        for (let page of pages) {
+            cansitatePages.push({ page, tur:slug })
+        }
+    }
+
+    return cansitatePages.map(m => {
+        const { page, tur } = m
+
+        return { tur, slug: ['sayfa', page.toString()] }
+    })
+
+
+}
+
+
+
+
+function paginate(array, page, pageSize) {
+    --page; // Adjusting to zero-based index
+
+    const startIndex = page * pageSize;
+    const endIndex = startIndex + pageSize;
+
+    return array.slice(startIndex, endIndex);
 }
